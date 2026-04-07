41e4ea47a97022d4ec9de0b93e3f66b417dd3e62
Zuberiya <zuberiya139@gmail.com>
2026-04-07 22:16:14 +0530

    changes

--- Downloads/therapy_care/app/page.tsx
+++ Downloads/therapy_care/app/page.tsx
@@ -1,32 +1,22 @@
-// import Navbar from "@/components/Navbar";
-// import Hero from "@/components/Hero";
-// import About from "./about/page";
-// import Services from "./services/page";
-// import Testimonials from "@/components/Testimonials";
-// import Footer from "@/components/Footer";
-// import ContactPage from "./contact/page";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import About from "./about/page";
+import Services from "./services/page";
+import Testimonials from "@/components/Testimonials";
+import Footer from "@/components/Footer";
+import ContactPage from "./contact/page";
 
-// export default function Home() {
-//   return (
-//     <main className="">
-//       <Navbar />
-//       <Hero />
-//       <About />
-//       <Services />
-//       <Testimonials />
-//       <ContactPage/>
-//       <Footer />
-//       {/* <h1 className="">APPLICATION ERROR..</h1> */}
-//     </main>
-//   );
-// }
-"use client";
-
-export default function Error() {
+export default function Home() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>500 - Server Error</h1>
-      <p>Something went wrong. Please try again later.</p>
-    </div>
+    <main className="">
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Testimonials />
+      <ContactPage/>
+      <Footer />
+      {/* <h1 className="">APPLICATION ERROR..</h1> */}
+    </main>
   );
 }
